7c6b99a2dc367ef8cc80fc976f1cd0640503c0ad
Ramadan Daudala <daudalaramadan2@gmail.com>
2025-07-21 00:48:32 +0000

    Add client and server folders to repo

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
+
+// Initialize Express app
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// app.use('/api/quiz', require('./routes/quiz'));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
