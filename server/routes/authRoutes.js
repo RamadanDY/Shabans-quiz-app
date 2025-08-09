a7542122c6ed05b87f9ea22750d2cf111f4d0c98
Ramadan Daudala <daudalaramadan2@gmail.com>
2025-08-09 02:08:46 +0000

    feat: Refactor CreateQuiz component to CreateTopicForm with topic creation functionality and validation

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const authController = require('../controller/authController');
-const authMiddleware = require('../middleware/authMiddleware');
+const protect = require('../middleware/authMiddleware');
+const adminMiddleware = require('../middleware/adminMiddleware');
+
 
 const router = express.Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.get('/me', authMiddleware, authController.getMe);
-router.get('/users', authMiddleware, authController.getAllUsers); // Added route for listing users
+// router.get('/me', authMiddleware, authController.getMe);
+// router.get('/users', authMiddleware, authController.getAllUsers); // Added route for listing users
+router.get('/me', protect, authController.getMe); // ✅ Any logged-in user
+router.get('/users', protect, adminMiddleware, authController.getAllUsers); // ✅ Admin only
 
 module.exports = router;
+
+
