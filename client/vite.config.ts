94719847895174e0d508332bbb3d659c1bb67dc0
Ramadan Daudala <daudalaramadan2@gmail.com>
2025-08-19 03:11:58 +0000

    lets do some small debugging

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // plugins: [react()],
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
